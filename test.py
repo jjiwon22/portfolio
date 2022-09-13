0ff1097ed47ca5a2af6506538975ad0663a1c72b
jjiwon22 <ssenzool20gmail.com>
2022-09-13 22:45:11 +0900

    부트캠프 숙제 제출

---
+++ test.py
@@ -0,0 +1,49 @@
+#1.
+#도형 넓이 구하기
+class shape object:
+    def __int__ (self, width, height):
+#사각형 넓이 구하기
+    def squre:
+        self, width() * self, height()
+#삼각형 넓이 구하기
+    def triangle:
+        self, width() * self, height()       
+#원 넓이 구하기
+    def circle:
+        self, width() * self, width()/4*math.pi        
+#2.
+# 계산기 만들기
+global_number = 0
+class calculator:
+    #더하기
+    def add(self. inp):
+        global global_number    
+        global_number = global_number + inp
+    #빼기
+    def sub(self. inp):
+        global global_number
+        global_number = global_number - inp
+    #곱하기
+    def mul(self. inp):
+        global global_number
+        global_number = global_number * inp
+    #나누기
+    def div(self. inp):
+        global global_number
+        global_number = global_number / inp       
+#3
+#프로필 관리 기능 만들어보기
+class profile()
+    def __inif__(self):
+        self.profile = {}
+#각각의 출력값 메소드 설정하기
+def set_profile(self, profile):
+    self.profile = profile
+profile = profile()    
+print(profile.get_name()) # 이름 출력
+print(profile.get_gender()) # 성별 출력
+print(profile.get_birthday()) # 생일 출력
+print(profile.get_age()) # 나이 출력
+print(profile.get_phone()) # 핸드폰번호 출력
+print(profile.email()) # 이메일 출력
+        
